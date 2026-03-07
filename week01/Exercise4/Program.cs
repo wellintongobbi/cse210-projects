ae72487d259ac8c17c933fc8b70e2c2604550173
wellintongobbi <wellintongobbi@gmail.com>
2026-03-07 11:29:34 -0300

    ex 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+             Console.Write("Enter a number (0 to quit): ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average of the numbers is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The maximum number is: {max}");
+
+        int min_positive = 0;
+        foreach (int number in numbers)
+        {
+            if (number > 0 && (min_positive == 0 || number < min_positive))
+            {
+                min_positive = number;
+            }
+        }
+        Console.WriteLine($"The minimum positive number is: {min_positive}");
     }
 }
