1a86a8f6239db7a3831744e58331234b0dfd5f33
wellintongobbi <wgobbi@byupathway.edu>
2026-04-14 19:39:08 -0300

    w7

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("03 Nov 2022", 30, 4.8));
+        activities.Add(new Cycling("04 Nov 2022", 45, 20.0));
+        activities.Add(new Swimming("05 Nov 2022", 30, 40));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,22 @@
+public class Running : Activity
+{
+    private double _distance; // km
+
+    public Running(string date, int minutes, double distance)
+        : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance() => _distance;
+
+    public override double GetSpeed()
+    {
+        return (_distance / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / _distance;
+    }
+}
